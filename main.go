e7bd0b4fd1ac5a60969e899d0d59b068b6bd986b
Ser1212 <sergionajm@gmail.com>
2023-07-17 14:21:34 -0400

    added .env API retreival

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	weatherAPIkey := os.Getenv("WEATHER_API_KEY")
+
+	fmt.Println(weatherAPIkey)
 
 	locationPtr := flag.String("location", "", "The name of the location for which you would like to check the weather")
 
